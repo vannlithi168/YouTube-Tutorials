b7cbae781d7a253aec84a6b669e71e4d4e10ad9a
027 <anbschool0027@027s-Mac-mini.local>
2023-11-13 20:01:02 +0700

    finish register form front end

---
+++ davegrey-mernauth/src/Login.js
@@ -0,0 +1,42 @@
+import { useRef, useState, useEffect } from "react";
+
+function Login() {
+  const userRef = useRef();
+  const errRef = useRef();
+
+  const [user, setUser] = useState("");
+  const [pwd, setPwd] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [success, setSuccess] = useState(false);
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, [user, pwd]);
+
+  return (
+    <section>
+      <p
+        ref={errRef}
+        className={errMsg ? "errmsg" : "offscreen"}
+        aria-live="assertive"
+      >
+        {errMsg}
+      </p>
+      <h1>Sign in</h1>
+      <form>
+        <label htmlFor="username">username</label>
+        <input
+          type="text"
+          id="username"
+          ref={userRef}
+          autoComplete="off"
+          onChange={(e) => setUser(e.target.value)}
+          value={user}
+          required
+        />
+      </form>
+    </section>
+  );
+}
+
+export default Login;
