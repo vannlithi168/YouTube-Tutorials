f97f1f449ab55b4d692624e79c7675fe39c67dd1
027 <anbschool0027@027s-Mac-mini.local>
2023-11-16 10:07:53 +0700

    create user model

--- cyberwolve-mernauth/server/db.js
+++ cyberwolve-mernauth/server/db.js
@@ -1,15 +1,11 @@
 const mongoose = require("mongoose");
 
-module.exports = () => {
-  const connectionParams = {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  };
+module.exports = async () => {
   try {
-    mongoose.connect(process.env.DB, connectionParams);
+    await mongoose.connect(process.env.DB);
     console.log("Connected to database successfully");
   } catch (error) {
-    console.log(error);
-    console.log("Could not connect database!");
+    console.error("Error connecting to the database:", error.message);
+    console.log("Could not connect to the database!");
   }
 };
